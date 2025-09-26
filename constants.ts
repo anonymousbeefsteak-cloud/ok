61744bcddd4c11b1d7adabc58fdf5e59c464f99b
anonymousbeefsteak-cloud <anonymousbeefsteak@gmail.com>
2025-09-26 08:39:07 +0800

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,5 +1,3 @@
 export const SHIPPING_FEE = 30;
 
-// FIX: Add missing GOOGLE_SHEETS_SCRIPT_URL constant.
-// Replace with your actual Google Apps Script URL.
 export const GOOGLE_SHEETS_SCRIPT_URL = '';
